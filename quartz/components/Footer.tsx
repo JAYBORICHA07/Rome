d505c5f84bc459ea882a82ce8332fa60bd93ce59
JAYBORICHA07 <jayboricha707@gmail.com>
2024-12-23 12:00:45 +0530

    updted all.

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,6 +1,5 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
 import { i18n } from "../i18n"
 
 interface Options {
@@ -13,13 +12,9 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
-            <li>
+            <li key={link}>
               <a href={link}>{text}</a>
             </li>
           ))}
